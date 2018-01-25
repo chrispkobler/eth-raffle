6cc3202db995e293e754dc75790ba521e9d131a4
Christopher Kobler <c.kobler@gmx.net>
2018-01-25 20:59:37 +0100

    Update compile.js

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Raffle.sol");
-const source = fs.readFileSync(lotteryPath, "utf-8");
+const rafflePath = path.resolve(__dirname, "contracts", "Raffle.sol");
+const source = fs.readFileSync(rafflePath, "utf-8");
 
 module.exports = solc.compile(source, 1).contracts[":Raffle"];
