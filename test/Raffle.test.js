9c308bab4e01c5f4f6af495ff689683a54dca874
Christopher Kobler <c.kobler@gmx.net>
2018-01-25 22:07:15 +0100

    Update Raffle test

--- test/Raffle.test.js
+++ test/Raffle.test.js
@@ -23,4 +23,82 @@ describe("Raffle Contract", () => {
   it("deploys contract", () => {
     assert.ok(raffle.options.address);
   });
+
+  it("allows one account to enter", async () => {
+    await raffle.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.01", "ether")
+    });
+
+    const players = await raffle.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it("allows multiple accounts to enter", async () => {
+    await raffle.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.01", "ether")
+    });
+    await raffle.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.01", "ether")
+    });
+    await raffle.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.01", "ether")
+    });
+
+    const players = await raffle.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+
+    assert.equal(3, players.length);
+  });
+
+  it("requires minimum amount of ether to enter", async () => {
+    try {
+      await raffle.methods.enter().send({
+        from: accounts[0],
+        value: 0
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("only allows manager to pick winner", async () => {
+    try {
+      await raffle.methods.pickWinner().send({ from: accounts[0] });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("it send ether to winner and resets players array", async () => {
+    await raffle.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("2", "ether")
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await raffle.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    const players = await raffle.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert(difference > web3.utils.toWei("1.8", "ether"));
+    assert.equal(0, players.length);
+  });
 });
